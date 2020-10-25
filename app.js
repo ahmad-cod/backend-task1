70666939c86fcaf1c3225c8560ec391513ff543f
aroyehunahmad <ahmadadewale800@gmail.com>
2020-10-24 20:30:34 -0700

    made palindrome act as a function edited output ..

--- app.js
+++ app.js
@@ -3,14 +3,12 @@
 function palindrome(str) {
     for( let i = 0; i < Math.ceil(str.length / 2);  i++ ) {
         if(str[i] !== str[str.length - i - 1]) {
-            console.log(false)
             return false
         }
     } 
-    console.log(true)
     return true
 }
 
-palindrome("ada")
-palindrome("dada")
-palindrome("madam")
+console.log("ada", palindrome("ada"));
+console.log("dada", palindrome("dada"));
+console.log("madam", palindrome("madam"));
